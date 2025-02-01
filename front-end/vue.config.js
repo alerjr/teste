7ce5e7a22ab65a83511460d0efd6e7c234566465
alerjr <alessandro.rocjr@gmail.com>
2025-01-31 21:05:11 -0300

    Adicionando arquivos para deploy no GitHub Pages

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -2,3 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  publicPath: '/supabase/frontend/', // Substitua pelo nome do seu repositório
+};
